f807fde3b6b6e1c639dd6fd8e21a4d1d4edb0c02
webo <boy_520girl@126.com>
2017-04-04 17:34:25 +0800

    update jquery to use info

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -28,6 +28,12 @@
 </template>
 
 <script>
+$(function(){
+	$('#navbar li').on('click',function(){
+		$('#navbar li').removeClass('active');
+		$(this).addClass('active');
+	});
+});
 export default {
   name: 'navbar',
   data () {
